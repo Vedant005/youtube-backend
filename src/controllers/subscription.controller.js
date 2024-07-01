8bbb4e4fb6e4b81a001841855930afc794626a1c
Vedant005 <vedantkanekar909@gmail.com>
2024-07-01 18:53:31 +0530

    changes in subscription controller

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -9,11 +9,39 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    if(!isValidObjectId(channelId)){
+        throw new ApiError(400,"Invalid channel id")
+    }
+
+    const isSubscribed = await Subscription.findOne({
+        subscriber:req.user._id,
+        channel : channelId
+    })
+
+    if(isSubscribed){
+
+        await Subscription.findByIdAndDelete(isSubscribed?._id)
+
+        return res
+               .status(200)
+               .json(new ApiResponse(200,"Unsubscirbed!"))
+    }
+
+    await Subscription.create({
+        subscriber:req.user._id,
+        channel:channelId
+    })
+
+    return res
+           .status(200)
+           .json(200,"Subscribed!")
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+
+    
 })
 
 // controller to return channel list to which user has subscribed
