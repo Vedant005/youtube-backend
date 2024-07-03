566dadbb74fc468fab4fccd30349de2c982745f8
Vedant005 <vedantkanekar909@gmail.com>
2024-07-03 16:15:06 +0530

    tweet controller changes

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -8,14 +8,84 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
     //TODO: create tweet
+    const {content} = req.body;
+
+    if(!content){
+        throw new ApiError(400,"Some content is required to make a tweet")
+    }
+
+    const create = await Tweet.create({
+        content:content,
+        owner: req.user._id
+    })
+
+    if(!create){
+        throw new ApiError(400,"Unable to make a tweet")
+    }
+
+    return res
+          .status(200)
+          .json(new ApiResponse(200,create,"Tweet created !"))
 })
 
 const getUserTweets = asyncHandler(async (req, res) => {
     // TODO: get user tweets
+
+    const {userId}= req.params;
+
+    const user = await User.findById(userId)
+
+    if(!user){
+        throw new ApiError(400,"User not found")
+
+        }
+    
+    const userTweets = await Tweet.aggregate([
+        {
+            $match:{
+                owner: new mongoose.Types.ObjectId(userId)
+            }
+        },
+        {
+             $lookup:{
+                from:""
+             }
+        }
+    ])
+
 })
 
 const updateTweet = asyncHandler(async (req, res) => {
     //TODO: update tweet
+    const {content} = req.body
+    const {tweetId}= req.params;
+
+    if(!content){
+        throw new ApiError(400,"Tweet not found!")
+    }
+    if(!tweetId){
+        throw new ApiError(400,"Invalid tweet id")
+    }
+
+    const newTweet = await Tweet.findByIdAndUpdate(
+        tweetId,
+        {
+            $set:{
+                content:content,
+            }
+        },
+        {new:true}
+    )
+
+    if(!newTweet){
+        throw new ApiError(400,"Could not update tweet")
+
+    }
+
+    return res
+          .status(200)
+          .json(new ApiResponse(200,newTweet,"Tweet updated!"))
+
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
