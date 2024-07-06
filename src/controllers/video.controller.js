b2e0197fc0c3dc269fcee24bee9a2dcbaaf709f3
Vedant005 <vedantkanekar909@gmail.com>
2024-07-06 20:40:24 +0530

    video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -71,6 +71,18 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: get video by id
+  if (!isValidObjectId(videoId)) {
+    throw new ApiError(400, "Invalid video id");
+  }
+
+  const video = await Video.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {},
+  ]);
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
